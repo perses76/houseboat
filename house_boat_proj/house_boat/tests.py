d7cf318a25571ab9a18c4d998958ffad0dedb8fd
Wadim Ovcharenko <wadim@veles-soft.com>
2013-11-10 12:29:27 +0200

    Created home view.

--- house_boat_proj/house_boat/tests.py
+++ house_boat_proj/house_boat/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+class HomeViewTest(TestCase):
+
+    def setUp(self):
+        self.url = reverse('home')
+
+    def test_get_success(self):
+        c = Client()
+        response = c.get(self.url)
+        self.assertEqual(response.status_code, 200)
